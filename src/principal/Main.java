5d8a712c1e04eb93ba4ae3d20210b472ec0a27aa
Lohrann Ferrari <lohrannfac@hotmail.com>
2022-01-30 14:12:17 -0300

    curso e mentoria java poo

---
+++ src/principal/Main.java
@@ -0,0 +1,35 @@
+package principal;
+
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+public class Main {
+
+	public static void main(String[] args) {	
+	 
+	 Curso curso1 = new Curso();
+     curso1.setTitulo("curso Java");
+	 curso1.setDescricao("descricao curso java");
+	 curso1.setCargaHoraria(8);
+	
+	 
+	 Curso curso2 = new Curso();
+     curso2.setTitulo("curso Js");
+	 curso2.setDescricao("descricao curso js");
+	 curso2.setCargaHoraria(4);
+	 
+	 Mentoria mentoria = new Mentoria();
+	 mentoria.setTitulo("mentoria de Java");
+	 mentoria.setDescricao("descricao mentoria Java");
+	 mentoria.setData(LocalDate.now());
+	 
+	 
+	 
+	 System.out.println(curso1);
+	 System.out.println(curso2);
+	 System.out.println(mentoria);;
+	}
+
+}
